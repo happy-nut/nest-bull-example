50a201cea872e7db3244f8239b024c77391eef9b
Hyungsun Song <happynut.dev@gmail.com>
2021-01-20 11:57:27 +0900

    Add method 4 and 5

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -24,51 +24,95 @@ export class TaskModule implements OnModuleInit {
     });
     await this.showJobs();
 
-    // 20초 동안 실행되는 job 추가.
-    const job = await this.taskQueue.add('heavy', 20);
+    // 최소 30초 동안 실행되는 job 추가.
+    const job = await this.taskQueue.add('heavy', 30);
     await this.showJobs();
 
-    // 목표 Process 에서 lock 이 걸려있는 job 을 종료시켜 보자.
+    // 목표: Process 에서 lock 이 걸려있는 job 을 종료시켜 보자.
 
     // 방법 1. Remove 호출.
-    // this.methodOne(job);
+    // this.method1(job);
     // 결과 1: 실패. Unhandled promise rejection 에러
 
     // 방법 2. JobPromise.cancel() 호출.
-    // this.methodTwo(job);
+    // this.method2(job);
     // 결과 2: 실패. this.jobIdPromiseMap[job.id] 가 undefined 라고 한다. 왜지?
 
     // 방법 3. moveToCompleted & remove 호출.
-    this.methodThree(job);
+    // this.method3(job);
     // 결과 3. onQueueRemoved 가 호출되긴 하는데, 원래 있던 job 다 끝나고 나서 Missing lock for job {job.id} failed 라는 에러
     //  그 이후에는 OnQueueDrained 가 호출됨.
     // 옵저버 패턴을 이용하면 process 까지 abort 시킬 수 있을 듯?
+
+    // 방법 4. discard & moveToCompleted 호출.
+    // this.method4(job);
+    // 결과 4.
+    /*
+     * OnQueueWaiting 3
+     * Processing 3 for 20 seconds
+     * OnQueueActive 3
+     * discard & moveToCompleted
+     * Processing done 3
+     * OnQueueError Error: Missing lock for job 3 failed
+     * OnQueueDrained
+     * */
+
+    // 방법 5. moveToCompleted & discard 호출.
+    this.method5(job);
+    // 결과 5.
+    /*
+     * OnQueueWaiting 5
+     * Processing 5 for 40 seconds
+     * OnQueueActive 5
+     * moveToCompleted & discard
+     * Processing done 5
+     * OnQueueError Error: Missing lock for job 5 failed
+     * OnQueueDrained
+     * */
   }
 
-  private methodThree(job: Job) {
+  private method5(job: Job) {
+    setTimeout(async () => {
+      console.log('moveToCompleted & discard ');
+      await job.moveToCompleted();
+      await job.discard();
+      await this.showJobs();
+    }, 4000);
+  }
+
+  private method4(job: Job) {
+    setTimeout(async () => {
+      console.log('discard & moveToCompleted');
+      await job.discard();
+      await job.moveToCompleted();
+      await this.showJobs();
+    }, 4000);
+  }
+
+  private method3(job: Job) {
     setTimeout(async () => {
       console.log('moveToCompleted & remove');
       await job.moveToCompleted();
       await job.remove();
       await this.showJobs();
-    }, 2000);
+    }, 4000);
   }
 
-  private methodTwo(job: Job) {
+  private method2(job: Job) {
     setTimeout(async () => {
       this.jobIdPromiseMap[job.id].cancel();
       await this.showJobs();
 
       await job.moveToCompleted();
       await this.showJobs();
-    }, 5000);
+    }, 4000);
   }
 
-  private methodOne(job: Job) {
+  private method1(job: Job) {
     setTimeout(async () => {
       await job.remove();
       await this.showJobs();
-    }, 2000);
+    }, 4000);
   }
 
   private async cleanOldJobs() {
